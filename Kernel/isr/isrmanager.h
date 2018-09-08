bb2719933bcfb2dc8a2bea0d8f23ce32ad39f0b6
dpatoukas <d.patoukas@gmail.com>
2018-09-08 19:48:23 +0200

    added licence

--- Kernel/isr/isrmanager.h
+++ Kernel/isr/isrmanager.h
@@ -1,5 +1,28 @@
-/*
- * isrmanager.h
+// This file is part of InK.
+// 
+// author = "Kasım Sinan Yıldırım " 
+// maintainer = "Kasım Sinan Yıldırım "
+// email = "sinanyil81 [at] gmail.com" 
+//  
+// copyright = "Copyright 2018 Delft University of Technology" 
+// license = "LGPL" 
+// version = "3.0" 
+// status = "Production"
+//
+// 
+// InK is free software: you ca	n redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>./*
+ /* isrmanager.h
  *
  *  Created on: 19 Feb 2018
  *
