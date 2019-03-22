68855c638292ba9438fcab71a22eb268c5645e88
Kasim Sinan YILDIRIM <sinanyil81@users.noreply.github.com>
2019-03-22 12:39:39 +0300

    Update timer.h
    Compilation error: solved

--- Kernel/timer/timer.h
+++ Kernel/timer/timer.h
@@ -21,7 +21,8 @@
 // GNU General Public License for more details.
 // 
 // You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.#ifndef INC_TMR_
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#ifndef INC_TMR_
 #define INC_TMR_
 
 //persistent timers "emulation"
